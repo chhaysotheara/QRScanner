851592353e4eee779ed5e7e055d3fef7ec0e0138
raa-jr <chhaysotheara11@gmail.com>
2016-12-12 16:01:17 +0700

    First Step QRScan succeed.

--- QRScanner/ViewController.swift
+++ QRScanner/ViewController.swift
@@ -7,20 +7,70 @@
 //
 
 import UIKit
+import AVFoundation
+import QRCodeReader
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,QRCodeReaderViewControllerDelegate{
+    
+    lazy var reader = QRCodeReaderViewController(builder: QRCodeReaderViewControllerBuilder {
+        $0.reader          = QRCodeReader(metadataObjectTypes: [AVMetadataObjectTypeQRCode])
+        $0.showTorchButton = true
+    })
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("hello world")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func Scanbtn(_ sender: Any) {
+        if QRCodeReader.supportsMetadataObjectTypes() {
+            reader.modalPresentationStyle = .formSheet
+            reader.delegate               = self
+            
+            reader.completionBlock = { (result: QRCodeReaderResult?) in
+                if let result = result {
+                    print("Completion with result: \(result.value) of type \(result.metadataType)")
+                }
+            }
+            
+            present(reader, animated: true, completion: nil)
+        }
+        else {
+            let alert = UIAlertController(title: "Error", message: "Reader not supported by the current device", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            
+            present(alert, animated: true, completion: nil)
+        }
+    }//end button
+    
+    // MARK: - QRCodeReader Delegate Methods
+    
+    func reader(_ reader: QRCodeReaderViewController, didScanResult result: QRCodeReaderResult) {
+        reader.stopScanning()
+        
+        dismiss(animated: true) { [weak self] in
+            let alert = UIAlertController(
+                title: "QRCodeReader",
+                message: String (format:"%@ (of type %@)", result.value, result.metadataType),
+                preferredStyle: .alert
+            )
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            
+            self?.present(alert, animated: true, completion: nil)
+        }
     }
-
+    
+    func reader(_ reader: QRCodeReaderViewController, didSwitchCamera newCaptureDevice: AVCaptureDeviceInput) {
+        if let cameraName = newCaptureDevice.device.localizedName {
+            print("Switching capturing to: \(cameraName)")
+        }
+    }
+    
+    func readerDidCancel(_ reader: QRCodeReaderViewController) {
+        reader.stopScanning()
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
 
 }
 
